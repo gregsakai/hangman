a36aa499a2da529b3e9cfe635f83986803d0ae94
gregsakai <gregsakai@gmail.com>
2018-02-02 10:09:12 -0800

    added alphabet buttons

--- main.js
+++ main.js
@@ -5,10 +5,21 @@ var numGuesses = 0;
 
 var wordBank = ["AQUARIUM", "CONVERTIBLE", "COMPUTER", "DYNAMITE", "KEYBOARD", "MAGAZINE", "SHELL", "VEHICLE", "HIGHWAY", "PROGRAM"];
 
-function guessLetter(){
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+function loadAlphabet() {
+  for (var i = 0; i < alphabet.length; i++) {
+    var letterButtons = document.createElement("button");
+    letterButtons.innerHTML = alphabet[i];
+    document.body.appendChild(letterButtons);
+    console.log("created buttons");
+  }
+}
+
+function guessLetter() {
   numGuesses++;
   console.log(numGuesses);
-  if(numGuesses > 7){
+  if (numGuesses > 7) {
     console.log("Game over");
     numGuesses = 0;
   }
